250d24da4c0023e1f0d5a816e5854a1c3f121f0f
Xavi Peña <xavipena@gmail.com>
2024-04-03 18:01:49 +0200

    forms

--- Blogs/FormTranslate.Designer.cs
+++ Blogs/FormTranslate.Designer.cs
@@ -32,6 +32,7 @@
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             this.btnes_ES = new System.Windows.Forms.Button();
             this.btnca_ES = new System.Windows.Forms.Button();
             this.dgvTranslations = new System.Windows.Forms.DataGridView();
@@ -49,10 +50,10 @@
             this.btnes_ES.ForeColor = System.Drawing.Color.White;
             this.btnes_ES.Image = ((System.Drawing.Image)(resources.GetObject("btnes_ES.Image")));
             this.btnes_ES.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnes_ES.Location = new System.Drawing.Point(2, 77);
-            this.btnes_ES.Margin = new System.Windows.Forms.Padding(2);
+            this.btnes_ES.Location = new System.Drawing.Point(3, 95);
+            this.btnes_ES.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnes_ES.Name = "btnes_ES";
-            this.btnes_ES.Size = new System.Drawing.Size(152, 37);
+            this.btnes_ES.Size = new System.Drawing.Size(232, 46);
             this.btnes_ES.TabIndex = 5;
             this.btnes_ES.Text = "Castellà";
             this.btnes_ES.UseVisualStyleBackColor = true;
@@ -67,10 +68,10 @@
             this.btnca_ES.ForeColor = System.Drawing.Color.White;
             this.btnca_ES.Image = ((System.Drawing.Image)(resources.GetObject("btnca_ES.Image")));
             this.btnca_ES.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnca_ES.Location = new System.Drawing.Point(2, 36);
-            this.btnca_ES.Margin = new System.Windows.Forms.Padding(2);
+            this.btnca_ES.Location = new System.Drawing.Point(3, 44);
+            this.btnca_ES.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnca_ES.Name = "btnca_ES";
-            this.btnca_ES.Size = new System.Drawing.Size(152, 37);
+            this.btnca_ES.Size = new System.Drawing.Size(232, 46);
             this.btnca_ES.TabIndex = 4;
             this.btnca_ES.Text = "Català";
             this.btnca_ES.UseVisualStyleBackColor = true;
@@ -78,44 +79,46 @@
             // 
             // dgvTranslations
             // 
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.AliceBlue;
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.Gray;
+            this.dgvTranslations.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
             this.dgvTranslations.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvTranslations.BackgroundColor = System.Drawing.Color.BlueViolet;
             this.dgvTranslations.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.dgvTranslations.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.BackColor = System.Drawing.Color.Violet;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.White;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.DarkViolet;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.White;
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dgvTranslations.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
-            this.dgvTranslations.ColumnHeadersHeight = 25;
-            this.dgvTranslations.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
             dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = System.Drawing.Color.BlueViolet;
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.MediumOrchid;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.DarkViolet;
             dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.White;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dgvTranslations.DefaultCellStyle = dataGridViewCellStyle2;
-            this.dgvTranslations.Dock = System.Windows.Forms.DockStyle.Left;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvTranslations.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvTranslations.ColumnHeadersHeight = 35;
+            this.dgvTranslations.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.BlueViolet;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.MediumOrchid;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvTranslations.DefaultCellStyle = dataGridViewCellStyle3;
             this.dgvTranslations.EnableHeadersVisualStyles = false;
             this.dgvTranslations.Location = new System.Drawing.Point(0, 0);
-            this.dgvTranslations.Margin = new System.Windows.Forms.Padding(2);
+            this.dgvTranslations.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dgvTranslations.Name = "dgvTranslations";
             this.dgvTranslations.RowHeadersVisible = false;
             this.dgvTranslations.RowHeadersWidth = 51;
-            dataGridViewCellStyle3.BackColor = System.Drawing.Color.SteelBlue;
-            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.White;
-            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.MediumBlue;
-            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.White;
-            this.dgvTranslations.RowsDefaultCellStyle = dataGridViewCellStyle3;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.AliceBlue;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.Color.MediumBlue;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.Color.White;
+            this.dgvTranslations.RowsDefaultCellStyle = dataGridViewCellStyle4;
             this.dgvTranslations.RowTemplate.Height = 24;
             this.dgvTranslations.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvTranslations.Size = new System.Drawing.Size(711, 440);
+            this.dgvTranslations.Size = new System.Drawing.Size(918, 542);
             this.dgvTranslations.TabIndex = 3;
             // 
             // RightMenu
@@ -124,20 +127,21 @@
             this.RightMenu.Controls.Add(this.btnca_ES);
             this.RightMenu.Controls.Add(this.btnes_ES);
             this.RightMenu.Dock = System.Windows.Forms.DockStyle.Right;
-            this.RightMenu.Location = new System.Drawing.Point(716, 0);
+            this.RightMenu.Location = new System.Drawing.Point(925, 0);
+            this.RightMenu.Margin = new System.Windows.Forms.Padding(4);
             this.RightMenu.Name = "RightMenu";
-            this.RightMenu.Size = new System.Drawing.Size(156, 440);
+            this.RightMenu.Size = new System.Drawing.Size(238, 542);
             this.RightMenu.TabIndex = 6;
             // 
             // FormTranslate
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Silver;
-            this.ClientSize = new System.Drawing.Size(872, 440);
+            this.ClientSize = new System.Drawing.Size(1163, 542);
             this.Controls.Add(this.RightMenu);
             this.Controls.Add(this.dgvTranslations);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "FormTranslate";
             this.Text = "Translation";
             ((System.ComponentModel.ISupportInitialize)(this.dgvTranslations)).EndInit();
